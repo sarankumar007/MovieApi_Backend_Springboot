9ee562f87091bdb8e494a1ec4b970444e4fe0145
sarankumar007 <sarankumar.r0076@gmail.com>
2023-05-22 03:28:36 +0530

    first commit

---
+++ src/main/java/com/sk/movies/ReviewService.java
@@ -0,0 +1,27 @@
+package com.sk.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class ReviewService {
+    @Autowired
+    ReviewRepository reviewRepository;
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody,String imdbId){
+        Review review=reviewRepository.insert(new Review(reviewBody, LocalDateTime.now(),LocalDateTime.now()));
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviews").value(review))
+                .first();
+
+                return review;
+    }
+}
